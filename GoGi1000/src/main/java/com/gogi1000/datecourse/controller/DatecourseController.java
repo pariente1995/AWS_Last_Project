6fe64aab25518ec65a1a475e5c92325c8d25d011
pariente1995 <pariente@naver.com>
2022-12-27 23:42:22 +0900

    aside 관련 수정_20221227_박세혁

--- GoGi1000/src/main/java/com/gogi1000/datecourse/controller/DatecourseController.java
+++ GoGi1000/src/main/java/com/gogi1000/datecourse/controller/DatecourseController.java
@@ -1,5 +1,21 @@
 package com.gogi1000.datecourse.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+@RequestMapping("/datecourse")
 public class DatecourseController {
 
+    // 데이트 코스 등록 화면으로 이동
+    @RequestMapping("/insertDatecourse")
+    public ModelAndView insertDatecourseView() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("datecourse/insertDatecourse.html");
+
+        return mv;
+    }
+
+    // 데이트 코스 등록
 }
