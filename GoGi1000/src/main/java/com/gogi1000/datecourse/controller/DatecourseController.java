da93ff5d30ca6db6afc6527fee92d6a0707b2c37
fur21 <hello.git.jang960222@naver.com>
2022-12-28 11:07:58 +0900

    댓글리스트, 좋아요리스트 UI

--- GoGi1000/src/main/java/com/gogi1000/datecourse/controller/DatecourseController.java
+++ GoGi1000/src/main/java/com/gogi1000/datecourse/controller/DatecourseController.java
@@ -1,5 +1,32 @@
 package com.gogi1000.datecourse.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+@RequestMapping("/datecourse")
 public class DatecourseController {
 
+    // 데이트 코스 등록 화면으로 이동
+    @GetMapping("/insertDatecourse")
+    public ModelAndView insertDatecourseView() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("datecourse/insertDatecourse.html");
+
+        return mv;
+    }
+
+    // 데이트 코스 등록
+
+    // 데이트 코스 수정 화면으로 이동
+    @GetMapping("/updateDatecourse")
+    public ModelAndView updateDatecourseView() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("datecourse/updateDatecourse.html");
+
+        return mv;
+    }
+    
 }
