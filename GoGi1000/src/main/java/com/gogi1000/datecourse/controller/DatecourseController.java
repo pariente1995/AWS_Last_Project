7caefa406984a9306c401e4f3703f7a1924581c5
pariente1995 <pariente@naver.com>
2022-12-28 11:35:14 +0900

    충돌 복구

--- GoGi1000/src/main/java/com/gogi1000/datecourse/controller/DatecourseController.java
+++ GoGi1000/src/main/java/com/gogi1000/datecourse/controller/DatecourseController.java
@@ -1,9 +1,16 @@
 package com.gogi1000.datecourse.controller;
 
+import com.gogi1000.datecourse.dto.DatecourseDTO;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/datecourse")
@@ -19,6 +26,60 @@ public class DatecourseController {
     }
 
     // 데이트 코스 등록
+    @PostMapping("/insertDatecourse")
+    public void insertDatecourse(DatecourseDTO datecourseDTO, MultipartFile[] uploadFiles, HttpServletResponse response, HttpServletRequest request) throws IOException {
+//        Datecourse datecourse = Datecourse.builder()
+//                .datecourseNm(datecourseDTO.getDatecourseNm())
+//                .datecourseArea(datecourseDTO.getDatecourseArea())
+//                .datecourseCategory(datecourseDTO.getDatecourseCategory())
+//                .datecourseSummary(datecourseDTO.getDatecourseSummary())
+//                .datecourseDesc(datecourseDTO.getDatecourseDesc())
+//                .datecourseAddr(datecourseDTO.getDatecourseAddr())
+//                .datecourseAddr(datecourseDTO.getDatecourseAddr())
+//                .datecourseInoutYn(datecourseDTO.getDatecourseInoutYn())
+//                .datecourseOfficialSite(datecourseDTO.getDatecourseOfficialSite())
+//                .datecou
+//
+//
+//                .boardContent(datecourseDTO.getBoardContent())
+//                .boardWriter(datecourseDTO.getBoardWriter())
+//                .boardRegdate(LocalDateTime.now())
+//                .build();
+//
+//        // DB에 입력될 파일 정보 리스트
+//        List<BoardFile> uploadFileList = new ArrayList<BoardFile>();
+//
+//        if(uploadFiles.length > 0) {
+//            System.out.println(request.getSession().getServletContext().getRealPath("/"));
+//            String attachPath = request.getSession().getServletContext().getRealPath("/")
+//                    + "/upload/";
+//
+//            File directory = new File(attachPath);
+//
+//            // 디렉토리가 존재하지 않으면 폴더 생성
+//            if(!directory.exists()) {
+//                directory.mkdir();
+//            }
+//
+//            // multipartFile 형식의 데이터를 DB 테이블에 맞는 구조로 변경
+//            for(int i=0; i<uploadFiles.length; i++) {
+//                MultipartFile file = uploadFiles[i];
+//
+//                // 빈칸이 아니고 널이 아닐 때만 uploadFileList에 담기
+//                if(!file.getOriginalFilename().equals("") && file.getOriginalFilename() != null) {
+//                    BoardFile boardFile = new BoardFile();
+//
+//                    boardFile = FileUtils.parseFileInfo(file, attachPath);
+//
+//                    uploadFileList.add(boardFile);
+//                }
+//            }
+//        }
+//        boardService.insertBoard(board, uploadFileList);
+
+        // 데이트 코스 리스트 화면으로 이동
+        //response.sendRedirect("/board/boardList");
+    }
 
     // 데이트 코스 수정 화면으로 이동
     @GetMapping("/updateDatecourse")
---
+++ GoGi1000/src/main/java/com/gogi1000/datecourse/dto/DatecourseDTO.java
@@ -0,0 +1,32 @@
+package com.gogi1000.datecourse.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor // 기본 생성자
+@AllArgsConstructor // 모든 매개변수를 받는 생성자
+@Builder // 객체 생성
+public class DatecourseDTO {
+    private int datecourseNo;
+    private String datecourseNm;
+    private String datecourseArea;
+    private String datecourseCategory;
+    private String datecourseSummary;
+    private String datecourseDesc;
+    private String datecourseAddr;
+    private String datecourseInoutYn;
+    private String datecourseFoodType;
+    private String datecourseTel;
+    private String datecourseOfficialSite;
+    private String datecourseParkingYn;
+    private String datecoursePetYn;
+    private int datecourseCnt;
+    // 자바스크립트, 자바단까지는 String 으로 처리 후 DB 에서 Date 형식으로 변환해준다.
+    // 자바 -> 자바스크립트, 자바스크립트 -> 자바로 넘길 때 Date 형식 처리가 매우 불편하다.
+    private String datecourseRgstDate;
+    private String datecourseModfDate;
+    private String datecourseUseYn;
+}
