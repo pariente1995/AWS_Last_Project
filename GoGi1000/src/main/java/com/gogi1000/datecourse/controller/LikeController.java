8f7ff756b1b54f163ac869b32a5d02c1cd167cda
fur21 <hello.git.jang960222@naver.com>
2022-12-28 11:23:49 +0900

    댓글리스트, 좋아요리스트 UI 설정

--- GoGi1000/src/main/java/com/gogi1000/datecourse/controller/LikeController.java
+++ GoGi1000/src/main/java/com/gogi1000/datecourse/controller/LikeController.java
@@ -1,5 +1,20 @@
 package com.gogi1000.datecourse.controller;
 
-public class LikeController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
+@RestController
+@RequestMapping("/like")
+public class LikeController {
+	
+	// 마이페이지 - 좋아요리스트 화면으로 이동
+    @GetMapping("/mypageLikeList")
+    public ModelAndView insertDatecourseView() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("like/mypageLikeList.html");
+
+        return mv;
+    }
 }
--- GoGi1000/src/main/java/com/gogi1000/datecourse/controller/ReviewController.java
+++ GoGi1000/src/main/java/com/gogi1000/datecourse/controller/ReviewController.java
@@ -1,5 +1,20 @@
 package com.gogi1000.datecourse.controller;
 
-public class ReviewController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
+@RestController
+@RequestMapping("/review")
+public class ReviewController {
+	
+	// 관리자페이지 - 댓글리스트 화면으로 이동
+    @GetMapping("/adminpageComment")
+    public ModelAndView insertDatecourseView() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("review/adminpageComment.html");
+
+        return mv;
+    }
 }
