59d14c1c7b49f30d8b73f1d0dabeaf86bd880764
dowon913 <dokang913@naver.com>
2023-01-10 17:14:34 +0900

    오류해결_2023-01-10_김도원

--- GoGi1000/src/main/java/com/gogi1000/datecourse/service/my/impl/MyDatecourseServiceImpl.java
+++ GoGi1000/src/main/java/com/gogi1000/datecourse/service/my/impl/MyDatecourseServiceImpl.java
@@ -5,11 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.gogi1000.datecourse.dto.MyDatecourseDTO;
-import com.gogi1000.datecourse.entity.Datecourse;
+import com.gogi1000.datecourse.common.CamelHashMap;
 import com.gogi1000.datecourse.entity.MyDatecourse;
 import com.gogi1000.datecourse.entity.MyDatecourseId;
-import com.gogi1000.datecourse.entity.Review;
 import com.gogi1000.datecourse.repository.MyDatecourseRepository;
 import com.gogi1000.datecourse.service.my.MyDatecourseService;
 
@@ -19,8 +17,9 @@ public class MyDatecourseServiceImpl implements MyDatecourseService {
 	@Autowired
 	MyDatecourseRepository myDatecourseRepository;
 	
+
 	@Override
-	public List<Datecourse> getMyDatecourseList(MyDatecourse myDatecourse) {
+	public List<CamelHashMap> getMyDatecourseList(MyDatecourse myDatecourse) {
 		return myDatecourseRepository.getMyDatecourseList(myDatecourse);
 	}
 	
